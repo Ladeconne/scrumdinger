1fdf3a7a2b94d393b1aeab12da54e2238355647b
ladeconne <hurdequintyves@gmail.com>
2024-01-12 15:51:00 +0100

    init scrumdinger

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Yves Bill on 11/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
